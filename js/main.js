4159fb5da5496888cd0ed2032b2199f729d8f569
AlexeyAA13 <99764427+AlexeyAA13@users.noreply.github.com>
2022-02-24 21:09:26 +0300

    fix bags

--- js/main.js
+++ js/main.js
@@ -1,51 +1,18 @@
-
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  if (min > max) {
+    throw new Error('ошибка!');
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
-
-
-
+getRandomIntInclusive(13, 5);
+checkCommentLength(5, 13);
 
 function checkCommentLength(string, maxLength) {
-    if (string.length <= maxLength) {
-        return true
-    } else {
-        return false
-    }
+  if (string.length <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
